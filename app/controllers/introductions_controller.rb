59659b7ba37873447686e512bd2de1fce1dc80c5
EC2 Default User <ec2-user@ip-172-31-42-39.ap-southeast-1.compute.internal>
2018-02-11 05:25:40 +0000

    page composition

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -1,14 +1,63 @@
 class IntroductionsController < ApplicationController
-  def index
+  def userInformation
   end
-  def user_information
+  
+  def main
+    # A - 메인 페이지
+  end
+  def mainEvent
+    # A1 - Event
+  end
+  def privacyInformation
+    # A3 - 개인정보
+  end
+  def agreeOfUtil
+    # A4 - 이용 약관
+  end
+  
+  def introduction
+    # B - 소개
+  end
+  def alumni
+    # B1 - 동창회
   end
   def studentActivity
-  end
-  def attendance
-  end
-  def introduction
+    # B2 = 학생활동 or 학생생활
   end
   def village
+    # B3 -  빌리지
   end
+  def club
+    # B4 - 동아리
+  end
+  def attendance
+    # B5 - 회원소개
+  end
+  def operationCommittee
+    # B6 - 운영위원
+  end
+  
+  def help
+    # F1 - help
+  end
+  def question
+    # F2 - 문의하기
+  end
+  def tier
+    # F3 - tier 확인
+  end
+  def payment
+    # F4 - 총창회비 납부 확인
+  end
+  def makePublic
+    # F5 - 정보 공개
+  end
+  def knupManager
+    # F6 - 홈페이지 관리
+  end
+  
+  def contactUs
+    # G - contact us
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,31 @@ Rails.application.routes.draw do
     resources :nreplies, only: [:create, :destroy]
   end
   devise_for :users, :controllers => { omniauth_callbacks: 'user/omniauth_callbacks' }
-  get '/introductions/user_information', to: 'introductions#user_information', as: 'user_information'
+  get '/introductions/userInformation', to: 'introductions#userInformation', as: 'userInformation'
+  
+  # A
+  root 'introductions#main'
+  get '/introductions/mainEvent', to: 'introductions#mainEvent', as: 'mainEvent'
+  get '/introductions/privacyInformation', to: 'introductions#privacyInformation', as: 'privacyInformation'
+  get '/introductions/agreeOfUtil', to: 'introductions#agreeOfUtil', as: 'agreeOfUtil'
+  
+  # B
+  get '/introductions/introduction', to: 'introductions#introduction', as: 'introduction'
+  get '/introductions/alumni', to: 'introductions#alumni', as: 'alumni'
   get '/introductions/studentActivity', to: 'introductions#studentActivity', as: 'studentActivity'
   get '/introductions/village', to: 'introductions#village', as: 'village'
-  get '/introductions/introduction', to: 'introductions#introduction', as: 'introduction'
+  get '/introductions/club', to: 'introductions#club', as: 'club'
   get '/introductions/attendance', to: 'introductions#attendance', as: 'attendance'
-  root 'introductions#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/introductions/operationCommittee', to: 'introductions#operationCommittee', as: 'operationCommittee'
+  
+  # F
+  get '/introductions/help', to: 'introductions#help', as: 'help'
+  get '/introductions/question', to: 'introductions#question', as: 'question'
+  get '/introductions/tier', to: 'introductions#tier', as: 'tier'
+  get '/introductions/payment', to: 'introductions#payment', as: 'payment'
+  get '/introductions/makePublic', to: 'introductions#makePublic', as: 'makePublic'
+  get '/introductions/knupManager', to: 'introductions#knupManager', as: 'knupManager'
+  
+  # G
+  get '/introductions/contactUs', to: 'introductions#contactUs', as: 'contactUs'
 end
