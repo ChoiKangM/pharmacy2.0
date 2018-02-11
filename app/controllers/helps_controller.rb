cd7211091e7f4a4683da1368c67638fafc992c67
EC2 Default User <ec2-user@ip-172-31-42-39.ap-southeast-1.compute.internal>
2018-02-11 05:57:58 +0000

    A1 mainEvent

---
+++ app/controllers/helps_controller.rb
@@ -0,0 +1,25 @@
+class HelpsController < ApplicationController
+  
+  def help
+    # F1 - help
+  end
+  def question
+    # F2 - 문의하기
+  end
+  def tier
+    # F3 - tier 확인
+  end
+  def payment
+    # F4 - 총창회비 납부 확인
+  end
+  def makePublic
+    # F5 - 정보 공개
+  end
+  def knupManager
+    # F6 - 홈페이지 관리
+  end
+  
+  def contactUs
+    # G - contact us
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,31 +9,31 @@ Rails.application.routes.draw do
     resources :nreplies, only: [:create, :destroy]
   end
   devise_for :users, :controllers => { omniauth_callbacks: 'user/omniauth_callbacks' }
-  get '/introductions/userInformation', to: 'introductions#userInformation', as: 'userInformation'
+  get '/introductions/userInformation', as: 'userInformation'
   
   # A
   root 'introductions#main'
-  get '/introductions/mainEvent', to: 'introductions#mainEvent', as: 'mainEvent'
-  get '/introductions/privacyInformation', to: 'introductions#privacyInformation', as: 'privacyInformation'
-  get '/introductions/agreeOfUtil', to: 'introductions#agreeOfUtil', as: 'agreeOfUtil'
+  get '/introductions/mainEvent', as: 'mainEvent'
+  get '/introductions/privacyInformation', as: 'privacyInformation'
+  get '/introductions/agreeOfUtil', as: 'agreeOfUtil'
   
   # B
-  get '/introductions/introduction', to: 'introductions#introduction', as: 'introduction'
-  get '/introductions/alumni', to: 'introductions#alumni', as: 'alumni'
-  get '/introductions/studentActivity', to: 'introductions#studentActivity', as: 'studentActivity'
-  get '/introductions/village', to: 'introductions#village', as: 'village'
-  get '/introductions/club', to: 'introductions#club', as: 'club'
-  get '/introductions/attendance', to: 'introductions#attendance', as: 'attendance'
-  get '/introductions/operationCommittee', to: 'introductions#operationCommittee', as: 'operationCommittee'
+  get '/introductions/introduction', as: 'introduction'
+  get '/introductions/alumni', as: 'alumni'
+  get '/introductions/studentActivity', as: 'studentActivity'
+  get '/introductions/village', as: 'village'
+  get '/introductions/club', as: 'club'
+  get '/introductions/attendance', as: 'attendance'
+  get '/introductions/operationCommittee', as: 'operationCommittee'
   
   # F
-  get '/introductions/help', to: 'introductions#help', as: 'help'
-  get '/introductions/question', to: 'introductions#question', as: 'question'
-  get '/introductions/tier', to: 'introductions#tier', as: 'tier'
-  get '/introductions/payment', to: 'introductions#payment', as: 'payment'
-  get '/introductions/makePublic', to: 'introductions#makePublic', as: 'makePublic'
-  get '/introductions/knupManager', to: 'introductions#knupManager', as: 'knupManager'
+  get '/helps/help', as: 'help'
+  get '/helps/question', as: 'question'
+  get '/helps/tier', as: 'tier'
+  get '/helps/payment', as: 'payment'
+  get '/helps/makePublic', as: 'makePublic'
+  get '/helps/knupManager', as: 'knupManager'
   
   # G
-  get '/introductions/contactUs', to: 'introductions#contactUs', as: 'contactUs'
+  get '/helps/contactUs', as: 'contactUs'
 end
